fffc793efeaa4d3a633196371f094487584879b3
TijayajHsohg <156028048+TijayajHsohg@users.noreply.github.com>
2024-01-09 03:54:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // JavaScript code to display "Hello, World!"
 document.write("<h1>Hello, World!</h1>");
+var name ='Hello';
+let name1='World1';
+const name12=[1,2,3];
+console.log(..name+..name1+"  "+..name12];
